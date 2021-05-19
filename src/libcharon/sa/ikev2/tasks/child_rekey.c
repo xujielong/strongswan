3dd7df5b8c7fd9e70839e89e46d647971f97bb36
Tobias Brunner <tobias@strongswan.org>
2021-05-19 12:00:39 +0200

    child-rekey: Maintain per-CPU values during rekeying

--- src/libcharon/sa/ikev2/tasks/child_rekey.c
+++ src/libcharon/sa/ikev2/tasks/child_rekey.c
@@ -213,6 +213,10 @@ METHOD(task_t, build_i, status_t,
 						this->child_sa->get_if_id(this->child_sa, FALSE));
 	this->child_create->use_label(this->child_create,
 						this->child_sa->get_label(this->child_sa));
+	this->child_create->use_num_cpus(this->child_create,
+						this->child_sa->get_num_cpus(this->child_sa));
+	this->child_create->use_cpu(this->child_create,
+						this->child_sa->get_cpu(this->child_sa));
 
 	if (this->child_create->task.build(&this->child_create->task,
 									   message) != NEED_MORE)
@@ -277,6 +281,10 @@ METHOD(task_t, build_r, status_t,
 						this->child_sa->get_if_id(this->child_sa, FALSE));
 	this->child_create->use_label(this->child_create,
 						this->child_sa->get_label(this->child_sa));
+	this->child_create->use_num_cpus(this->child_create,
+						this->child_sa->get_num_cpus(this->child_sa));
+	this->child_create->use_cpu(this->child_create,
+						this->child_sa->get_cpu(this->child_sa));
 	config = this->child_sa->get_config(this->child_sa);
 	this->child_create->set_config(this->child_create, config->get_ref(config));
 	this->child_create->task.build(&this->child_create->task, message);
